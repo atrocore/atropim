6aa7dfa21aea0bead38ddcf787ca6f1cc4f5f7c8
r.ratsun <r.ratsun@gmail.com>
2020-10-12 13:26:50 +0300

    Add ability to add modules settings

--- client/modules/pim/src/views/settings/edit.js
+++ client/modules/pim/src/views/settings/edit.js
@@ -1,4 +1,20 @@
 Espo.define('pim:views/settings/edit', 'views/settings/edit', function (Dep) {
-    return Dep.extend({});
+
+    return Dep.extend({
+
+        scope: 'Settings',
+
+        recordView: 'pim:views/admin/settings',
+
+        setupHeader: function () {
+            this.createView('header', this.headerView, {
+                model: this.model,
+                el: '#main > .header',
+                template: 'pim:admin/settings/headers/settings'
+            });
+        }
+
+    });
+
 });
 
--- client/modules/pim/src/views/settings/record/edit.js
+++
@@ -1,4 +0,0 @@
-Espo.define('pim:views/settings/record/edit', 'views/settings/record/edit', function (Dep) {
-    return Dep.extend({});
-});
-
