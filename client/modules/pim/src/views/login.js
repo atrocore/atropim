e60197d61ebd6fa116caf7f66d6f141ec2332c05
r.ratsun <r.ratsun@gmail.com>
2020-10-16 15:12:14 +0300

    new logo

--- client/modules/pim/src/views/login.js
+++ client/modules/pim/src/views/login.js
@@ -34,7 +34,7 @@ Espo.define('pim:views/login', 'class-replace!pim:views/login',
         getLogoSrc: function () {
             const companyLogoId = this.getConfig().get('companyLogoId');
             if (!companyLogoId) {
-                return this.getBasePath() + 'client/modules/pim/img/treo_pim_logo_white.svg';
+                return this.getBasePath() + 'client/modules/pim/img/pim_logo_white.svg';
             }
             return this.getBasePath() + '?entryPoint=LogoImage&id='+companyLogoId+'&t=' + companyLogoId;
         }
