c16f330ebee9454e219da2e1cd2780f415673a6c
r.ratsun <r.ratsun@gmail.com>
2020-10-22 12:46:29 +0300

    .

--- client/modules/pim/src/views/associated-product/fields/related-product.js
+++ client/modules/pim/src/views/associated-product/fields/related-product.js
@@ -31,12 +31,9 @@
 Espo.define('pim:views/associated-product/fields/related-product', 'treo-core:views/fields/filtered-link',
     Dep => Dep.extend({
 
-        selectBoolFilterList:  ['notEntity', 'notAssociatedProducts'],
+        selectBoolFilterList:  ['notAssociatedProducts'],
 
         boolFilterData: {
-            notEntity() {
-                return this.model.get('mainProductId');
-            },
             notAssociatedProducts() {
                 return {mainProductId: this.model.get('mainProductId'), associationId: this.model.get('associationId')};
             }
