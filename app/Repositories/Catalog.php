ad34878b810dd185588f625630b73930c69faa69
Roman Zablodskyi <r.zablodskyi@gmail.com>
2020-12-08 14:29:45 +0200

    fixed translations, removed team inheritance

--- app/Repositories/Catalog.php
+++ app/Repositories/Catalog.php
@@ -52,4 +52,41 @@ class Catalog extends Base
         // remove catalog products
         $this->getEntityManager()->nativeQuery("UPDATE product SET deleted=1 WHERE catalog_id='$id'");
     }
+
+    /**
+     * @inheritDoc
+     */
+    protected function afterSave(Entity $entity, array $options = array())
+    {
+        parent::afterSave($entity, $options);
+
+        if ($entity->isAttributeChanged('assignedUserId') || $entity->isAttributeChanged('ownerUserId')) {
+            $assignedUserOwnership = $this->getConfig()->get('assignedUserProductOwnership', '');
+            $ownerUserOwnership = $this->getConfig()->get('ownerUserProductOwnership', '');
+
+            if ($assignedUserOwnership == 'fromCatalog' || $ownerUserOwnership == 'fromCatalog') {
+                foreach ($entity->get('products') as $product) {
+                    $toSave = false;
+
+                    if ($assignedUserOwnership == 'fromCatalog'
+                        && ($product->get('assignedUserId') == null || $product->get('assignedUserId') == $entity->getFetched('assignedUserId'))) {
+                        $product->set('assignedUserId', $entity->get('assignedUserId'));
+                        $product->set('assignedUserName', $entity->get('assignedUserName'));
+                        $toSave = true;
+                    }
+
+                    if ($ownerUserOwnership == 'fromCatalog'
+                        && ($product->get('ownerUserId') == null || $product->get('ownerUserId') == $entity->getFetched('ownerUserId'))) {
+                        $product->set('ownerUserId', $entity->get('ownerUserId'));
+                        $product->set('ownerUserName', $entity->get('ownerUserName'));
+                        $toSave = true;
+                    }
+
+                    if ($toSave) {
+                        $this->getEntityManager()->saveEntity($product, ['skipAll' => true]);
+                    }
+                }
+            }
+        }
+    }
 }
--- app/Repositories/ProductFamily.php
+++ app/Repositories/ProductFamily.php
@@ -130,4 +130,41 @@ class ProductFamily extends Base
             }
         }
     }
+
+    /**
+     * @inheritDoc
+     */
+    protected function afterSave(Entity $entity, array $options = array())
+    {
+        parent::afterSave($entity, $options);
+
+        if ($entity->isAttributeChanged('assignedUserId') || $entity->isAttributeChanged('ownerUserId')) {
+            $assignedUserOwnership = $this->getConfig()->get('assignedUserProductOwnership', '');
+            $ownerUserOwnership = $this->getConfig()->get('ownerUserProductOwnership', '');
+
+            if ($assignedUserOwnership == 'fromProductFamily' || $ownerUserOwnership == 'fromProductFamily') {
+                foreach ($entity->get('products') as $product) {
+                    $toSave = false;
+
+                    if ($assignedUserOwnership == 'fromProductFamily'
+                        && ($product->get('assignedUserId') == null || $product->get('assignedUserId') == $entity->getFetched('assignedUserId'))) {
+                        $product->set('assignedUserId', $entity->get('assignedUserId'));
+                        $product->set('assignedUserName', $entity->get('assignedUserName'));
+                        $toSave = true;
+                    }
+
+                    if ($ownerUserOwnership == 'fromProductFamily'
+                        && ($product->get('ownerUserId') == null || $product->get('ownerUserId') == $entity->getFetched('ownerUserId'))) {
+                        $product->set('ownerUserId', $entity->get('ownerUserId'));
+                        $product->set('ownerUserName', $entity->get('ownerUserName'));
+                        $toSave = true;
+                    }
+
+                    if ($toSave) {
+                        $this->getEntityManager()->saveEntity($product, ['skipAll' => true]);
+                    }
+                }
+            }
+        }
+    }
 }
