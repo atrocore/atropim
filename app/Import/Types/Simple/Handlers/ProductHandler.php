1fe9473eab80d3f0a05c4990bbd9f0c39d05fc1a
Roman Zablodskyi <r.zablodskyi@gmail.com>
2021-02-10 10:39:10 +0200

    Fixed search exists entities in import

--- app/Import/Types/Simple/Handlers/ProductHandler.php
+++ app/Import/Types/Simple/Handlers/ProductHandler.php
@@ -86,7 +86,7 @@ class ProductHandler extends AbstractHandler
         // find exists if it needs
         $exists = [];
         if (in_array($data['action'], ['update', 'create_update']) && !empty($idRow)) {
-            $exists = $this->getExists($entityType, $idRow['name'], array_column($fileData, $idRow['column']));
+            $exists = $this->getExists($entityType, $idRow, array_column($fileData, $idRow['column']));
         }
 
         // prepare file row
