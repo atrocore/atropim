3addcbfe9655372c978ad4564464e0410bf220d2
antebra <ab@atrocore.com>
2021-07-06 12:35:32 +0300

    .

---
+++ app/Migrations/V1Dot1Dot42.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * This file is part of AtroPIM.
+ *
+ * AtroPIM - Open Source PIM application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ * Website: https://atropim.com
+ *
+ * AtroPIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroPIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AtroPIM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "AtroPIM" word.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Migrations;
+
+/**
+ * Migration class for version 1.1.42
+ */
+class V1Dot1Dot42 extends V1Dot1Dot21
+{
+    public function up(): void
+    {
+        $this->execute("ALTER TABLE `category_asset` DROP channel");
+        $this->execute("DROP INDEX UNIQ_A3F321005DA19414584665A ON `product_asset`");
+    }
+
+    public function down(): void
+    {
+    }
+}
