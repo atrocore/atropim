612daa01d4cbf748cf492e34b5f442af7aaaf36b
Roman Zablodskyi <r.zablodskyi@gmail.com>
2021-07-06 12:13:08 +0300

    Override fields handlers for Product Attributes

---
+++ app/Core/Utils/FieldManager/Types/DefaultType.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * This file is part of EspoCRM and/or AtroCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * AtroCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ *
+ * AtroCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "AtroCore" word.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Core\Utils\FieldManager\Types;
+
+use Espo\ORM\Entity;
+
+/**
+ * Class DefaultType
+ */
+class DefaultType extends \Espo\Core\Utils\FieldManager\Types\DefaultType
+{
+    /**
+     * @inheritDoc
+     */
+    public function prepareSqlUniqueData(Entity $entity, string $field): array
+    {
+        $result = parent::prepareSqlUniqueData($entity, $field);
+
+        $result['where'] .= " AND product_attribute_value.attribute_id = '" .$entity->get('attributeId') . "'";
+
+        return $result;
+    }
+}
