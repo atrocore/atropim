45219830b3de41cccacdb637ac63643aa7f628ba
r.ratsun <r.ratsun@gmail.com>
2021-09-07 13:43:09 +0300

    .

--- app/Controllers/ProductFamily.php
+++ app/Controllers/ProductFamily.php
@@ -27,50 +27,10 @@
  * these Appropriate Legal Notices must retain the display of the "AtroPIM" word.
  */
 
+declare(strict_types=1);
+
 namespace Pim\Controllers;
 
-use Espo\Core\Exceptions;
-use Slim\Http\Request;
-
-/**
- * Class ProductFamily
- */
 class ProductFamily extends AbstractController
 {
-    /**
-     * Get count not empty product family attributes
-     *
-     * @ApiDescription(description="Get products count, linked with product family attribute")
-     * @ApiMethod(type="GET")
-     * @ApiRoute(name="/ProductFamily/{product_family_id}/productAttributesCount")
-     * @ApiParams(name="product_family_id", type="string", is_required=1, description="ProductFamily id")
-     * @ApiReturn(sample="'int'")
-     *
-     * @param array   $params
-     * @param array   $data
-     * @param Request $request
-     *
-     * @return int
-     *
-     * @throws Exceptions\BadRequest
-     * @throws Exceptions\Forbidden
-     */
-    public function actionProductsCount($params, $data, Request $request)
-    {
-        if (!$request->isGet()) {
-            throw new Exceptions\BadRequest();
-        }
-
-        if (!$this->getAcl()->check($this->name, 'read')) {
-            throw new Exceptions\Forbidden();
-        }
-
-        return $this
-            ->getEntityManager()
-            ->getRepository('ProductAttributeValue')
-            ->join('productFamilyAttribute')
-            ->join('product')
-            ->where(['productFamilyAttribute.id' => $request->get('attributeId'), 'product.deleted' => 0])
-            ->count();
-    }
 }
