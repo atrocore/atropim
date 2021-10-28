e893d33bc1e69713cae7d3098eab5056a88f9f48
r.ratsun <r.ratsun@gmail.com>
2021-10-28 13:34:07 +0300

    .

--- app/Repositories/Product.php
+++ app/Repositories/Product.php
@@ -556,63 +556,6 @@ class Product extends AbstractRepository
         }
     }
 
-    public function onProductFamilyChange(string $productId, ?string $prevPfId): void
-    {
-        if (empty($prevPfId)) {
-            return;
-        }
-
-        $mode = $this->getConfig()->get('behaviorOnProductFamilyChange', 'retainAllInheritedAttributes');
-
-        if ($mode == 'retainAllInheritedAttributes') {
-            return;
-        }
-
-        $pfas = $this
-            ->getEntityManager()
-            ->getRepository('ProductFamilyAttribute')
-            ->where(['productFamilyId' => $prevPfId])
-            ->find();
-
-        if (count($pfas) === 0) {
-            return;
-        }
-
-        $pavs = $this
-            ->getEntityManager()
-            ->getRepository('ProductAttributeValue')
-            ->where(['productId' => $productId, 'attributeId' => array_column($pfas->toArray(), 'attributeId')])
-            ->find();
-
-        if (count($pavs) === 0) {
-            return;
-        }
-
-        foreach ($pfas as $pfa) {
-            foreach ($pavs as $pav) {
-                if ($pav->get('attributeId') === $pfa->get('attributeId') && $pav->get('scope') === $pfa->get('scope') && $pav->get('isRequired') === $pfa->get('isRequired')) {
-                    if ($pfa->get('scope') === 'Channel' && $pav->get('channelId') !== $pfa->get('channelId')) {
-                        continue 1;
-                    }
-                    if ($mode === 'removeOnlyInheritedAttributesWithNoValue') {
-                        if ($pav->get('value') !== null) {
-                            continue 1;
-                        }
-                        if ($this->getConfig()->get('isMultilangActive', false)) {
-                            foreach ($this->getConfig()->get('inputLanguageList', []) as $locale) {
-                                if ($pav->get('value' . ucfirst(Util::toCamelCase(strtolower($locale)))) !== null) {
-                                    continue 2;
-                                }
-                            }
-                        }
-                    }
-
-                    $this->getEntityManager()->removeEntity($pav);
-                }
-            }
-        }
-    }
-
     public function save(Entity $entity, array $options = [])
     {
         if ($this->getEntityManager()->getPDO()->inTransaction()) {
@@ -678,7 +621,7 @@ class Product extends AbstractRepository
         }
 
         if ($entity->isAttributeChanged('productFamilyId')) {
-            $this->onProductFamilyChange($entity->get('id'), $entity->getFetched('productFamilyId'));
+            $this->onProductFamilyChange($entity);
         }
 
         if (!$entity->isNew() && $entity->isAttributeChanged('type')) {
@@ -827,6 +770,70 @@ class Product extends AbstractRepository
         parent::afterUnrelate($entity, $relationName, $foreign, $options);
     }
 
+    protected function onProductFamilyChange(Entity $product): void
+    {
+        if (empty($product->getFetched('productFamilyId'))) {
+            return;
+        }
+
+        $mode = $this->getConfig()->get('behaviorOnProductFamilyChange', 'retainAllInheritedAttributes');
+
+        if ($mode == 'retainAllInheritedAttributes') {
+            return;
+        }
+
+        $where = [
+            'productFamilyId' => $product->getFetched('productFamilyId')
+        ];
+
+        if (!empty($product->get('productFamilyId'))) {
+            $where['attributeId!='] = array_column($product->get('productFamily')->get('productFamilyAttributes')->toArray(), 'attributeId');
+        }
+
+        $pfas = $this
+            ->getEntityManager()
+            ->getRepository('ProductFamilyAttribute')
+            ->where($where)
+            ->find();
+
+        if (count($pfas) === 0) {
+            return;
+        }
+
+        $pavs = $this
+            ->getEntityManager()
+            ->getRepository('ProductAttributeValue')
+            ->where(['productId' => $product->get('id'), 'attributeId' => array_column($pfas->toArray(), 'attributeId')])
+            ->find();
+
+        if (count($pavs) === 0) {
+            return;
+        }
+
+        foreach ($pfas as $pfa) {
+            foreach ($pavs as $pav) {
+                if ($pav->get('attributeId') === $pfa->get('attributeId') && $pav->get('scope') === $pfa->get('scope') && $pav->get('isRequired') === $pfa->get('isRequired')) {
+                    if ($pfa->get('scope') === 'Channel' && $pav->get('channelId') !== $pfa->get('channelId')) {
+                        continue 1;
+                    }
+                    if ($mode === 'removeOnlyInheritedAttributesWithNoValue') {
+                        if ($pav->get('value') !== null) {
+                            continue 1;
+                        }
+                        if ($this->getConfig()->get('isMultilangActive', false)) {
+                            foreach ($this->getConfig()->get('inputLanguageList', []) as $locale) {
+                                if ($pav->get('value' . ucfirst(Util::toCamelCase(strtolower($locale)))) !== null) {
+                                    continue 2;
+                                }
+                            }
+                        }
+                    }
+
+                    $this->getEntityManager()->removeEntity($pav);
+                }
+            }
+        }
+    }
 
     /**
      * @param Entity $entity
