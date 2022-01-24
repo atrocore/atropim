d98b66dbf5be602f2d64d2599aaff7fc865f3207
r.ratsun <r.ratsun@gmail.com>
2022-01-24 14:31:18 +0200

    .

--- client/modules/pim/src/views/product/fields/image.js
+++ client/modules/pim/src/views/product/fields/image.js
@@ -29,13 +29,23 @@
 Espo.define('pim:views/product/fields/image', 'views/fields/image',
     Dep => Dep.extend({
 
+        setup() {
+            Dep.prototype.setup.call(this);
+
+            this.listenTo(this.model, 'after:unrelate', () => {
+                this.model.fetch();
+                this.reRender();
+            });
+        },
+
         afterRender() {
             Dep.prototype.afterRender.call(this);
-            if (this.mode === 'list')
-            {
+
+            if (this.mode === 'list') {
                 this.$el.find('img').css({'max-height': '120px', 'max-width': '100%'});
-            } else if(this.mode === 'detail')
-            {
+            }
+
+            if (this.mode === 'detail') {
                 this.$el.find('.attachment-preview').css({'display': 'block'});
                 this.$el.find('img').css({'display': 'block', 'margin': '0 auto'});
             }
