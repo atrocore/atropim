111b9bf7afcfad8e8e08efff1b70b365d8e39b43
r.ratsun <r.ratsun@gmail.com>
2022-05-06 17:32:26 +0300

    .

--- app/Migrations/V1Dot5Dot16.php
+++ app/Migrations/V1Dot5Dot16.php
@@ -40,11 +40,21 @@ class V1Dot5Dot16 extends Base
     public function up(): void
     {
         $this->exec("ALTER TABLE `attribute` ADD virtual_product_field TINYINT(1) DEFAULT '0' NOT NULL COLLATE utf8mb4_unicode_ci");
+
+        $path = 'custom/Espo/Custom/Resources/layouts/ProductAttributeValue/listSmall.json';
+        if (file_exists($path)) {
+            unlink($path);
+        }
     }
 
     public function down(): void
     {
         $this->exec("ALTER TABLE `attribute` DROP virtual_product_field");
+
+        $path = 'custom/Espo/Custom/Resources/layouts/ProductAttributeValue/listSmall.json';
+        if (file_exists($path)) {
+            unlink($path);
+        }
     }
 
     protected function exec(string $query): void
