451942ba96e97d75005e9f5eb0f6c82ba92d9f28
Roman Zablodskyi <rz@atrocore.com>
2022-07-21 09:41:12 +0300

    Fixed sql query

--- app/Repositories/ProductFamilyAttribute.php
+++ app/Repositories/ProductFamilyAttribute.php
@@ -85,10 +85,12 @@ class ProductFamilyAttribute extends Base
      */
     public function getAvailableChannelsForPavs(string $productFamilyId): array
     {
+        $productFamilyId = $this->getPDO()->quote($productFamilyId);
+
         $sql = "SELECT p.id, pc.channel_id 
                 FROM product p 
                     LEFT JOIN product_channel pc on p.id = pc.product_id AND pc.deleted = 0 
-                WHERE p.product_family_id = '$productFamilyId' AND p.deleted = 0";
+                WHERE p.product_family_id = $productFamilyId AND p.deleted = 0";
 
         return $this->getPDO()->query($sql)->fetchAll(\PDO::FETCH_ASSOC|\PDO::FETCH_GROUP|\PDO::FETCH_COLUMN);
     }
