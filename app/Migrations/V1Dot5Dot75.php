8456a6ecfbf9beb93f66888d1172289a22e538c2
r.ratsun <r.ratsun@gmail.com>
2022-09-22 11:16:21 +0300

    .

---
+++ app/Migrations/V1Dot5Dot75.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of AtroPIM.
+ *
+ * AtroPIM - Open Source PIM application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ * Website: https://atropim.com
+ *
+ * AtroPIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroPIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AtroPIM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "AtroPIM" word.
+ *
+ * This software is not allowed to be used in Russia and Belarus.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Migrations;
+
+use Treo\Core\Migration\Base;
+
+class V1Dot5Dot75 extends Base
+{
+    /**
+     * @inheritDoc
+     */
+    public function up(): void
+    {
+        $this->getPDO()->exec("DELETE FROM product_channel WHERE channel_id IS NULL OR channel_id=''");
+        $this->getPDO()->exec("DELETE FROM product_channel WHERE product_id IS NULL OR product_id=''");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down(): void
+    {
+    }
+}
