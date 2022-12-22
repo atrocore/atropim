6241b7943251cd59c96fd6708103302a5f5fde39
r.ratsun <r.ratsun@gmail.com>
2022-12-22 14:52:59 +0200

    .

--- app/Migrations/V1Dot6Dot0.php
+++ app/Migrations/V1Dot6Dot0.php
@@ -33,9 +33,7 @@ declare(strict_types=1);
 
 namespace Pim\Migrations;
 
-use Doctrine\DBAL\Connection;
 use Espo\Core\Exceptions\BadRequest;
-use Espo\Core\Utils\Util;
 use Treo\Core\Migration\Base;
 
 class V1Dot6Dot0 extends Base
@@ -50,52 +48,40 @@ class V1Dot6Dot0 extends Base
             "CREATE UNIQUE INDEX UNIQ_BD38116AADFEE0E7B6E62EFAAF55D372F5A1AAD04DB71B5EB3B4E33 ON product_family_attribute (product_family_id, attribute_id, scope, channel_id, language, deleted)"
         );
 
-//        $records = $this->getSchema()->getConnection()->createQueryBuilder()
-//            ->select('pfa.*')
-//            ->from('product_family_attribute', 'pfa')
-//            ->leftJoin('pav', 'channel', 'c', 'pav.channel_id=c.id AND c.deleted=0');
-
-        // update updateInconsistentAttributes
-        // CheckProductAttributes JOB
-        // hasInconsistentAttributes
-
-        echo '<pre>';
-        print_r('123');
-        die();
-
         if ($this->getConfig()->get('isMultilangActive', false)) {
-
-            $pfas = $connection->createQueryBuilder()
-                ->select('*')
-                ->from('product_family_attribute')
-                ->where('language = :main')
-                ->setParameter('main', 'main')
+            $records = $this->getSchema()->getConnection()->createQueryBuilder()
+                ->select('pfa.*')
+                ->from('product_family_attribute', 'pfa')
+                ->leftJoin('pfa', 'attribute', 'a', 'pfa.attribute_id=a.id')
+                ->where('pfa.deleted=0')
+                ->andWhere('a.deleted=0')
+                ->andWhere('a.is_multilang=1')
                 ->fetchAllAssociative();
 
+            if (!empty($records)) {
+                $container = (new \Espo\Core\Application())->getContainer();
+                $auth = new \Espo\Core\Utils\Auth($container);
+                $auth->useNoAuth();
+                $service = $container->get('serviceFactory')->create('ProductFamilyAttribute');
+                foreach ($records as $record) {
+                    $attachment = new \stdClass();
+                    $attachment->languages = $this->getConfig()->get('inputLanguageList', []);
+                    $attachment->productFamilyId = $record['product_family_id'];
+                    $attachment->attributeId = $record['attribute_id'];
+                    $attachment->isRequired = !empty($record['is_required']);
+                    $attachment->scope = $record['scope'];
+                    $attachment->channelId = $record['channel_id'];
 
-            $locales = $this->getConfig()->get('inputLanguageList', []);
-            foreach ($pfas as $pfa) {
-                $pfa_locales = $locales;
-                if ($pfa['channel_id'] && !empty($channels[$pfa['channel_id']])) {
-                    $pfa_locales = $channels[$pfa['channel_id']];
-                }
-                foreach ($pfa_locales as $locale) {
-                    $pfa['id'] = Util::generateId();
-                    $pfa['language'] = $locale;
-
-                    $columns = [];
-                    foreach ($pfa as $key => $value) {
-                        $columns[$key] = ':' . $key;
+                    try {
+                        $service->createEntity($attachment);
+                    } catch (\Throwable $e) {
                     }
-
-                    $connection->createQueryBuilder()
-                        ->insert('product_family_attribute')
-                        ->values($columns)
-                        ->setParameters($pfa)
-                        ->executeQuery();
                 }
             }
         }
+
+        $this->exec("DELETE FROM job WHERE job.name='CheckProductAttributes'");
+        $this->exec("DELETE FROM scheduled_job WHERE scheduled_job.job='CheckProductAttributes'");
 
         $this->exec("DROP INDEX IDX_MAIN_LANGUAGE_ID ON product_attribute_value");
         $this->exec("ALTER TABLE product_attribute_value DROP main_language_id");
