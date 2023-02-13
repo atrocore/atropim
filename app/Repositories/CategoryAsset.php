ea3e617bb98d39fff22e2819c9676de7413c40e4
r.ratsun <r.ratsun@gmail.com>
2023-02-13 12:50:37 +0200

    .

---
+++ app/Repositories/CategoryAsset.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * This file is part of AtroPIM.
+ *
+ * AtroPIM - Open Source PIM application.
+ * Copyright (C) 2020 AtroCore UG (haftungsbeschränkt).
+ * Website: https://atropim.com
+ *
+ * AtroPIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AtroPIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AtroPIM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "AtroPIM" word.
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Repositories;
+
+use Espo\ORM\Entity;
+
+class CategoryAsset extends \Espo\Core\Templates\Repositories\Relationship
+{
+    protected function beforeSave(Entity $entity, array $options = [])
+    {
+        if ($entity->isNew() && $entity->get('sorting') === null) {
+            $last = $this->where(['categoryId' => $entity->get('categoryId')])->order('sorting', 'DESC')->findOne();
+            $entity->set('sorting', empty($last) ? 0 : (int)$last->get('sorting') + 10);
+        }
+
+        parent::beforeSave($entity, $options);
+    }
+
+    protected function afterSave(Entity $entity, array $options = [])
+    {
+        parent::afterSave($entity, $options);
+
+        if ($entity->isAttributeChanged('isMainImage') && !empty($entity->get('isMainImage'))) {
+            foreach ($this->where(['isMainImage' => true, 'categoryId' => $entity->get('categoryId'), 'id!=' => $entity->get('id')])->find() as $categoryAsset) {
+                $categoryAsset->set('isMainImage', false);
+                $this->getEntityManager()->saveEntity($categoryAsset);
+            }
+        }
+    }
+
+    public function updateSortOrder(array $ids): void
+    {
+        foreach ($ids as $k => $id) {
+            $id = $this->getPDO()->quote((string)$id);
+            $sortOrder = (int)$k * 10;
+            $this->getPDO()->exec("UPDATE `category_asset` SET sorting=$sortOrder WHERE id=$id");
+        }
+    }
+}
