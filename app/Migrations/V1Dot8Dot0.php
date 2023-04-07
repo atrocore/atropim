5a34d4837189839c13ca61ad566af7ce1829f1e7
r.ratsun <r.ratsun@gmail.com>
2023-04-07 11:47:07 +0300

    .

--- app/Migrations/V1Dot8Dot0.php
+++ app/Migrations/V1Dot8Dot0.php
@@ -38,9 +38,9 @@ class V1Dot8Dot0 extends Base
     public function up(): void
     {
         $this->getPDO()->exec("DROP TABLE IF EXISTS classification");
-        $this->getPDO()->exec("DROP TABLE IF EXISTS classification_attribute");
-
         $this->getPDO()->exec("ALTER TABLE product_family RENAME classification");
+
+        $this->getPDO()->exec("DROP TABLE IF EXISTS classification_attribute");
         $this->getPDO()->exec("ALTER TABLE product_family_attribute RENAME classification_attribute");
 
         $this->getPDO()->exec("DROP INDEX IDX_PRODUCT_FAMILY_ID ON classification_attribute");
@@ -54,7 +54,9 @@ class V1Dot8Dot0 extends Base
         $this->getPDO()->exec("ALTER TABLE product CHANGE product_family_id classification_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
         $this->getPDO()->exec("CREATE INDEX IDX_CLASSIFICATION_ID ON product (classification_id)");
 
+        $this->getPDO()->exec("DROP TABLE IF EXISTS product_classification");
         $this->getPDO()->exec("CREATE TABLE product_classification (id INT AUTO_INCREMENT NOT NULL UNIQUE COLLATE `utf8mb4_unicode_ci`, classification_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, product_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, deleted TINYINT(1) DEFAULT '0' COLLATE `utf8mb4_unicode_ci`, INDEX IDX_1602AC1D2A86559F (classification_id), INDEX IDX_1602AC1D4584665A (product_id), UNIQUE INDEX UNIQ_1602AC1D2A86559F4584665A (classification_id, product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB");
+
         $this->getPDO()->exec("DROP INDEX id ON product_classification");
 
         $products = $this->getPDO()->query("SELECT * FROM product WHERE classification_id Is NOT NULL AND deleted=0")->fetchAll(\PDO::FETCH_ASSOC);
