bee520318fea08290cf0984ae8cb3d2bc33db594
r.ratsun <r.ratsun@gmail.com>
2023-04-21 12:22:45 +0300

    .

--- app/Migrations/V1Dot8Dot3.php
+++ app/Migrations/V1Dot8Dot3.php
@@ -37,8 +37,42 @@ class V1Dot8Dot3 extends Base
 {
     public function up(): void
     {
-        $this->execute("ALTER TABLE attribute ADD extensible_enum_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
-        $this->execute("CREATE INDEX IDX_EXTENSIBLE_ENUM_ID ON attribute (extensible_enum_id)");
+//        $this->execute("ALTER TABLE attribute ADD extensible_enum_id VARCHAR(24) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
+//        $this->execute("CREATE INDEX IDX_EXTENSIBLE_ENUM_ID ON attribute (extensible_enum_id)");
+
+        $this->getPDO()->exec("DELETE FROM extensible_enum WHERE deleted=1");
+        $this->getPDO()->exec("DELETE FROM extensible_enum_option WHERE deleted=1");
+
+        $records = $this->getPDO()->query("SELECT a.* FROM attribute a WHERE a.type IN ('enum','multiEnum') AND a.deleted=0")->fetchAll(\PDO::FETCH_ASSOC);
+        foreach ($records as $record) {
+            $ids = @json_decode($record['type_value_ids'], true);
+            if (!empty($ids)) {
+                $values = @json_decode((string)$record['type_value'], true);
+                if (!empty($values)) {
+                    $this->execute("INSERT INTO extensible_enum (id,name) VALUES ('{$record['id']}','{$record['name']}')");
+                    foreach ($ids as $k => $id) {
+                        $value = isset($values[$k]) ? "'" . $values[$k] . "'" : 'NULL';
+                        $sortOrder = $k * 10;
+                        $this->execute("INSERT INTO extensible_enum_option (id,extensible_enum_id,name,sort_order) VALUES ('{$record['id']}_{$id}','{$record['id']}',$value, $sortOrder)");
+                    }
+
+                    if (!empty($this->getConfig()->get('isMultilangActive'))) {
+                        foreach ($this->getConfig()->get('inputLanguageList', []) as $v) {
+                            $locale = strtolower($v);
+                            $languageValues = @json_decode((string)$record['type_value_' . $locale], true);
+                            if (!empty($languageValues)) {
+                                foreach ($ids as $k => $id) {
+                                    $languageValue = isset($languageValues[$k]) ? "'" . $languageValues[$k] . "'" : 'NULL';
+                                    $this->execute("UPDATE extensible_enum_option SET name_{$locale}={$languageValue} WHERE id='{$record['id']}_{$id}'");
+                                }
+                            }
+                        }
+                    }
+
+                    $this->getPDO()->exec("UPDATE attribute SET extensible_enum_id='{$record['id']}' WHERE id='{$record['id']}'");
+                }
+            }
+        }
     }
 
     public function down(): void
