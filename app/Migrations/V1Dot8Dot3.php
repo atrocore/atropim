de4c67eeaa71fbe6a1c68f1dd4657570ada6ce6c
r.ratsun <r.ratsun@gmail.com>
2023-04-24 09:39:07 +0300

    .

--- app/Migrations/V1Dot8Dot3.php
+++ app/Migrations/V1Dot8Dot3.php
@@ -142,7 +142,9 @@ class V1Dot8Dot3 extends Base
         }
 
         $this->getPDO()->exec("UPDATE attribute SET type='extensibleEnum' WHERE type='enum' AND deleted=0");
+        $this->getPDO()->exec("UPDATE product_attribute_value SET attribute_type='extensibleEnum' WHERE attribute_type='enum' AND deleted=0");
         $this->getPDO()->exec("UPDATE attribute SET type='extensibleMultiEnum' WHERE type='multiEnum' AND deleted=0");
+        $this->getPDO()->exec("UPDATE product_attribute_value SET attribute_type='extensibleMultiEnum' WHERE attribute_type='multiEnum' AND deleted=0");
         $this->getPDO()->exec("ALTER TABLE attribute DROP is_sorted");
     }
 
