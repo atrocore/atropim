df1fcc42b3da2f4d9023f7a8a957935e634e8423
Raoul Dzoukou <rd@atrocore.com>
2023-05-16 16:47:46 +0100

    Range filter for Product Attributes

--- client/modules/pim/src/views/product/record/search.js
+++ client/modules/pim/src/views/product/record/search.js
@@ -28,171 +28,196 @@
 
 Espo.define('pim:views/product/record/search', 'views/record/search', Dep => Dep.extend({
 
-        events: _.extend({}, Dep.prototype.events, {
-            'click .dropdown-submenu > a[data-filter-name="addAttributeFilter"]': function (e) {
-                e.stopPropagation();
-                e.preventDefault();
+    events: _.extend({}, Dep.prototype.events, {
+        'click .dropdown-submenu > a[data-filter-name="addAttributeFilter"]': function (e) {
+            e.stopPropagation();
+            e.preventDefault();
 
-                this.$leftDropdown.click();
+            this.$leftDropdown.click();
 
-                const scope = 'Attribute';
-                if (this.getAcl().check(scope, 'read')) {
-                    const viewName = this.getMetadata().get(['clientDefs', scope, 'modalViews', 'select']) || 'views/modals/select-records';
-                    this.notify('Loading...');
-                    this.createView('dialog', viewName, {
-                        scope: scope,
-                        multiple: false,
-                        createButton: false,
-                        massRelateEnabled: false
-                    }, dialog => {
-                        dialog.render();
-                        this.notify(false);
-                        dialog.once('select', attribute => {
-                            let fieldParams = {
-                                filterView: 'pim:views/product/search/filter',
-                                isAttribute: true,
-                                label: attribute.get('name'),
-                                type: attribute.get('type')
-                            };
+            const scope = 'Attribute';
+            if (this.getAcl().check(scope, 'read')) {
+                const viewName = this.getMetadata().get(['clientDefs', scope, 'modalViews', 'select']) || 'views/modals/select-records';
+                this.notify('Loading...');
+                this.createView('dialog', viewName, {
+                    scope: scope,
+                    multiple: false,
+                    createButton: false,
+                    massRelateEnabled: false
+                }, dialog => {
+                    dialog.render();
+                    this.notify(false);
+                    dialog.once('select', attribute => {
+                        let fieldParams = {
+                            filterView: 'pim:views/product/search/filter',
+                            isAttribute: true,
+                            label: attribute.get('name'),
+                            type: attribute.get('type')
+                        };
 
-                            this.addFilter(attribute.id, {fieldParams: fieldParams});
-                        });
+                        this.addFilter(attribute.id, {fieldParams: fieldParams});
                     });
-                }
-            },
-            'click .advanced-filters a.remove-attribute-filter': function (e) {
-                e.stopPropagation();
-                e.preventDefault();
-
-                var $target = $(e.currentTarget);
-                var name = $target.data('name');
-
-                this.$el.find('a[data-id="' + name.split('-')[0] + '"]').parent().removeClass('hide');
-                var container = this.getView('filter-' + name).$el.closest('div.filter');
-
-                this.clearView('filter-' + name);
-                container.remove();
-                delete this.advanced[name];
-
-                if (name in this.pinned) {
-                    delete this.pinned[name];
-                }
-
-                this.presetName = this.primary;
-
-                this.fetch();
-                this.updateSearch();
-                this.toggleFilterActionsVisibility();
-                this.toggleResetVisibility();
-
-                this.manageLabels();
-                this.handleLeftDropdownVisibility();
-                this.setupOperatorLabels();
-
-                this.search();
+                });
             }
-        }),
-
-        setup() {
-            Dep.prototype.setup.call(this);
-
-            this.additionalFilters.push({
-                name: 'addAttributeFilter',
-                label: this.translate('addAttributeFilter', 'labels', 'Product')
-            });
         },
+        'click .advanced-filters a.remove-attribute-filter': function (e) {
+            e.stopPropagation();
+            e.preventDefault();
 
-        getFilterDataList: function () {
-            /**
-             * Collect attributes filters
-             */
-            let attributesIds = [];
-            let attributesFields = [];
-            $.each(this.advanced, (field, fieldData) => {
-                if (fieldData.fieldParams && fieldData.fieldParams.isAttribute) {
-                    attributesIds.push(field.split('-').shift());
-                    attributesFields.push(field);
-                }
-            });
+            var $target = $(e.currentTarget);
+            var name = $target.data('name');
 
-            /**
-             * Remove attributes filters if such attributes does not exist anymore
-             */
-            if (attributesIds.length > 0) {
-                const where = [{
-                    type: "in",
-                    attribute: "id",
-                    value: attributesIds
-                }];
-                this.ajaxGetRequest(`Attribute`, {where: where}, {async: false}).success(response => {
-                    if (response.list) {
-                        attributesIds.forEach((id, k) => {
-                            let found = false;
-                            response.list.forEach(record => {
-                                if (record.id === id) {
-                                    found = true;
-                                }
-                            });
-                            if (!found) {
-                                delete this.advanced[attributesFields[k]];
+            this.$el.find('a[data-id="' + name.split('-')[0] + '"]').parent().removeClass('hide');
+            var container = this.getView('filter-' + name).$el.closest('div.filter');
+
+            this.clearView('filter-' + name);
+            container.remove();
+            delete this.advanced[name];
+
+            if (name in this.pinned) {
+                delete this.pinned[name];
+            }
+
+            this.presetName = this.primary;
+
+            this.fetch();
+            this.updateSearch();
+            this.toggleFilterActionsVisibility();
+            this.toggleResetVisibility();
+
+            this.manageLabels();
+            this.handleLeftDropdownVisibility();
+            this.setupOperatorLabels();
+
+            this.search();
+        }
+    }),
+
+    setup() {
+        Dep.prototype.setup.call(this);
+
+        this.additionalFilters.push({
+            name: 'addAttributeFilter',
+            label: this.translate('addAttributeFilter', 'labels', 'Product')
+        });
+    },
+
+    getFilterDataList: function () {
+        /**
+         * Collect attributes filters
+         */
+        let attributesIds = [];
+        let attributesFields = [];
+        $.each(this.advanced, (field, fieldData) => {
+            if (fieldData.fieldParams && fieldData.fieldParams.isAttribute) {
+                attributesIds.push(field.split('-').shift());
+                attributesFields.push(field);
+            }
+        });
+
+        /**
+         * Remove attributes filters if such attributes does not exist anymore
+         */
+        if (attributesIds.length > 0) {
+            const where = [{
+                type: "in",
+                attribute: "id",
+                value: attributesIds
+            }];
+            this.ajaxGetRequest(`Attribute`, {where: where}, {async: false}).success(response => {
+                if (response.list) {
+                    attributesIds.forEach((id, k) => {
+                        let found = false;
+                        response.list.forEach(record => {
+                            if (record.id === id) {
+                                found = true;
                             }
                         });
-                    }
-                });
-            }
-
-            return Dep.prototype.getFilterDataList.call(this);
-        },
-
-        isFieldExist(name, filterField) {
-            if (filterField.fieldParams && filterField.fieldParams.isAttribute) {
-                return true;
-            }
-
-            return Dep.prototype.isFieldExist.call(this, name, filterField);
-        },
-
-        fetch: function () {
-            this.textFilter = (this.$el.find('input[name="textFilter"]').val() || '').trim();
-
-            this.bool = {};
-
-            this.boolFilterList.forEach(function (name) {
-                this.bool[name] = this.$el.find('input[name="' + name + '"]').prop('checked');
-            }, this);
-
-            for (let field in this.advanced) {
-                let view = this.getView('filter-' + field).getView('field');
-                this.advanced[field] = view.fetchSearch();
-
-                let fieldParams = view.options.searchParams.fieldParams || {};
-                if (fieldParams.isAttribute) {
-                    if (this.advanced[field] === false) {
-                        this.advanced[field] = {};
-                    }
-                    this.advanced[field]['fieldParams'] = fieldParams;
+                        if (!found) {
+                            delete this.advanced[attributesFields[k]];
+                        }
+                    });
                 }
-                view.searchParams = this.advanced[field];
-            }
-        },
-
-        updateCollection() {
-            const defaultFilters = Espo.Utils.cloneDeep(this.searchManager.get());
-
-            const list = this.getParentView();
-            const catalogTreePanel = list.getView('catalogTreePanel');
-            if (catalogTreePanel && catalogTreePanel.catalogTreeData) {
-                const extendedFilters = Espo.Utils.cloneDeep(defaultFilters);
-                $.each(catalogTreePanel.catalogTreeData, (key, value) => {
-                    extendedFilters[key] = _.extend({}, extendedFilters[key], value);
-                });
-                this.searchManager.set(extendedFilters);
-            }
-
-            Dep.prototype.updateCollection.call(this);
-
-            this.searchManager.set(defaultFilters);
+            });
         }
 
-    })
+        return Dep.prototype.getFilterDataList.call(this);
+    },
+
+    isFieldExist(name, filterField) {
+        if (filterField.fieldParams && filterField.fieldParams.isAttribute) {
+            return true;
+        }
+
+        return Dep.prototype.isFieldExist.call(this, name, filterField);
+    },
+
+    filterAdded(name) {
+        return !!Object.keys(this.advanced).find(k => k.startsWith(name + '-'))
+    },
+
+    addFilter(name, params) {
+        if (params.fieldParams && ['rangeFloat', 'rangeInt'].indexOf(params.fieldParams.type) >= 0) {
+            const fieldType = params.fieldParams.type;
+            const newType = fieldType === 'rangeFloat' ? 'float' : 'int';
+            const from = name + 'From'
+            const to = name + 'To'
+            const paramsFrom = {fieldParams: {...params.fieldParams, type: newType, label: params.fieldParams.label + ' From'}}
+            const paramsTo = {fieldParams: {...params.fieldParams, type: newType, label: params.fieldParams.label + ' To'}}
+            if (!this.filterAdded(from)) {
+                this.addFilter(from, paramsFrom)
+            }
+            if (!this.filterAdded(to)) {
+                this.addFilter(to, paramsTo)
+            }
+            return
+        }
+
+        return Dep.prototype.addFilter.call(this, name, params);
+    },
+
+    fetch: function () {
+        this.textFilter = (this.$el.find('input[name="textFilter"]').val() || '').trim();
+
+        this.bool = {};
+
+        this.boolFilterList.forEach(function (name) {
+            this.bool[name] = this.$el.find('input[name="' + name + '"]').prop('checked');
+        }, this);
+
+        for (let field in this.advanced) {
+            let view = this.getView('filter-' + field).getView('field');
+            this.advanced[field] = view.fetchSearch();
+
+            let fieldParams = view.options.searchParams.fieldParams || {};
+            if (fieldParams.isAttribute) {
+                if (this.advanced[field] === false) {
+                    this.advanced[field] = {};
+                }
+                this.advanced[field]['fieldParams'] = fieldParams;
+            }
+            view.searchParams = this.advanced[field];
+        }
+    },
+
+
+    updateCollection() {
+        const defaultFilters = Espo.Utils.cloneDeep(this.searchManager.get());
+
+        const list = this.getParentView();
+        const catalogTreePanel = list.getView('catalogTreePanel');
+        if (catalogTreePanel && catalogTreePanel.catalogTreeData) {
+            const extendedFilters = Espo.Utils.cloneDeep(defaultFilters);
+            $.each(catalogTreePanel.catalogTreeData, (key, value) => {
+                extendedFilters[key] = _.extend({}, extendedFilters[key], value);
+            });
+            this.searchManager.set(extendedFilters);
+        }
+
+        Dep.prototype.updateCollection.call(this);
+
+        this.searchManager.set(defaultFilters);
+    }
+
+})
 );
