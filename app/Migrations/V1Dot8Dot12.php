106eca27f837a9445e5d899d3511c99be3fe569a
r.ratsun <33658723+rratsun@users.noreply.github.com>
2023-06-30 15:16:38 +0300

    Update V1Dot8Dot12.php

--- app/Migrations/V1Dot8Dot12.php
+++ app/Migrations/V1Dot8Dot12.php
@@ -37,19 +37,27 @@ class V1Dot8Dot12 extends Base
 {
     public function up(): void
     {
-        $this->getPDO()->exec("ALTER TABLE product_attribute_value ADD int_value1 INT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
-        $this->getPDO()->exec("CREATE INDEX IDX_INT_VALUE1 ON product_attribute_value (int_value1, deleted)");
+        $this->exec("ALTER TABLE product_attribute_value ADD int_value1 INT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
+        $this->exec("CREATE INDEX IDX_INT_VALUE1 ON product_attribute_value (int_value1, deleted)");
 
-        $this->getPDO()->exec("ALTER TABLE product_attribute_value ADD float_value1 DOUBLE PRECISION DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
-        $this->getPDO()->exec("CREATE INDEX IDX_FLOAT_VALUE1 ON product_attribute_value (float_value, deleted)");
+        $this->exec("ALTER TABLE product_attribute_value ADD float_value1 DOUBLE PRECISION DEFAULT NULL COLLATE `utf8mb4_unicode_ci`");
+        $this->exec("CREATE INDEX IDX_FLOAT_VALUE1 ON product_attribute_value (float_value, deleted)");
     }
 
     public function down(): void
     {
-        $this->getPDO()->exec("DROP INDEX IDX_INT_VALUE1 ON product_attribute_value");
-        $this->getPDO()->exec("ALTER TABLE product_attribute_value DROP int_value1");
+        $this->exec("DROP INDEX IDX_INT_VALUE1 ON product_attribute_value");
+        $this->exec("ALTER TABLE product_attribute_value DROP int_value1");
 
-        $this->getPDO()->exec("DROP INDEX IDX_FLOAT_VALUE1 ON product_attribute_value");
-        $this->getPDO()->exec("ALTER TABLE product_attribute_value DROP float_value1");
+        $this->exec("DROP INDEX IDX_FLOAT_VALUE1 ON product_attribute_value");
+        $this->exec("ALTER TABLE product_attribute_value DROP float_value1");
+    }
+
+    protected function exec(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
     }
 }
