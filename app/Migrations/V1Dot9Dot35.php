f63423c95f41e9eb1e10b7444828b92ef4ce51c2
Roman Ratsun <rr@atrocore.com>
2023-08-22 08:52:11 +0000

    .

--- app/Migrations/V1Dot9Dot35.php
+++ app/Migrations/V1Dot9Dot35.php
@@ -36,7 +36,7 @@ class V1Dot9Dot35 extends Base
     public function up(): void
     {
         try {
-            $attributes = $this->getPDO()->query("SELECT * FROM `attribute` WHERE max_length IS NOT NULL AND deleted=0")->fetchAll(\PDO::FETCH_ASSOC);
+            $attributes = $this->getPDO()->query("SELECT * FROM `attribute` WHERE deleted=0")->fetchAll(\PDO::FETCH_ASSOC);
         } catch (\Throwable $e) {
             $attributes = [];
         }
@@ -46,11 +46,44 @@ class V1Dot9Dot35 extends Base
             if (!is_array($data)) {
                 $data = [];
             }
-            $data['field']['maxLength'] = $attribute['max_length'];
+
+            if (!empty($attribute['max_length'])) {
+                $data['field']['maxLength'] = $attribute['max_length'];
+            }
+
+            $data['field']['countBytesInsteadOfCharacters'] = !empty($attribute['count_bytes_instead_of_characters']);
+
             $this->exec("UPDATE `attribute` SET data='" . json_encode($data) . "' WHERE id='{$attribute['id']}'");
         }
 
         $this->exec("ALTER TABLE attribute DROP max_length");
+        $this->exec("ALTER TABLE attribute DROP count_bytes_instead_of_characters");
+
+        $this->exec("ALTER TABLE classification_attribute ADD data LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci` COMMENT '(DC2Type:jsonObject)'");
+
+        try {
+            $cas = $this->getPDO()->query("SELECT * FROM classification_attribute WHERE deleted=0")->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\Throwable $e) {
+            $cas = [];
+        }
+
+        foreach ($cas as $ca) {
+            $data = @json_decode((string)$ca['data'], true);
+            if (!is_array($data)) {
+                $data = [];
+            }
+
+            if (!empty($ca['max_length'])) {
+                $data['field']['maxLength'] = $ca['max_length'];
+            }
+
+            $data['field']['countBytesInsteadOfCharacters'] = !empty($ca['count_bytes_instead_of_characters']);
+
+            $this->exec("UPDATE classification_attribute SET data='" . json_encode($data) . "' WHERE id='{$ca['id']}'");
+        }
+
+        $this->exec("ALTER TABLE classification_attribute DROP max_length");
+        $this->exec("ALTER TABLE classification_attribute DROP count_bytes_instead_of_characters");
 
         $this->updateComposer("atrocore/pim", "^1.9.35");
     }
