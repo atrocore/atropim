1919d5efbcde42960003b28022e6acc2029e4005
Roman Ratsun <rr@atrocore.com>
2023-09-07 13:50:16 +0000

    .

--- app/Controllers/AbstractController.php
+++ app/Controllers/AbstractController.php
@@ -3,7 +3,7 @@
  * AtroCore Software
  *
  * This source file is available under GNU General Public License version 3 (GPLv3).
- * Full copyright and license information is available in LICENSE.md, located in the root directory.
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
  *
  * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
  * @license    GPLv3 (https://www.gnu.org/licenses/)
