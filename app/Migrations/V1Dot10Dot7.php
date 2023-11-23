b249b56d42a04ab2335161cd71c6da7f54de32d0
Roman Ratsun <rr@atrocore.com>
2023-11-23 10:41:41 +0000

    .

--- app/Migrations/V1Dot10Dot7.php
+++ app/Migrations/V1Dot10Dot7.php
@@ -17,12 +17,52 @@ class V1Dot10Dot7 extends Base
 {
     public function up(): void
     {
-        //  "name": "productAssets" to  "name": "assets" in custom
-        //        categoryAssets
-//        productChannels
+        $path = "custom/Espo/Custom/Resources/layouts/Product/relationships.json";
+        if (file_exists($path)) {
+            $contents = file_get_contents($path);
+            $contents = str_replace('"productAssets"', '"assets"', $contents);
+            $contents = str_replace('"productChannels"', '"channels"', $contents);
+            file_put_contents($path, $contents);
+        }
+
+        $path = "custom/Espo/Custom/Resources/layouts/Assets/relationships.json";
+        if (file_exists($path)) {
+            $contents = file_get_contents($path);
+            $contents = str_replace('"productAssets"', '"products"', $contents);
+            $contents = str_replace('"categoryAssets"', '"categories"', $contents);
+            file_put_contents($path, $contents);
+        }
+
+        $path = "custom/Espo/Custom/Resources/layouts/Channels/relationships.json";
+        if (file_exists($path)) {
+            $contents = file_get_contents($path);
+            $contents = str_replace('"productChannels"', '"products"', $contents);
+            file_put_contents($path, $contents);
+        }
+
+        $path = "custom/Espo/Custom/Resources/layouts/Category/relationships.json";
+        if (file_exists($path)) {
+            $contents = file_get_contents($path);
+            $contents = str_replace('"categoryAssets"', '"assets"', $contents);
+            file_put_contents($path, $contents);
+        }
+
+        $path = "custom/Espo/Custom/Resources/layouts/Asset/detailSmall.json";
+        if (file_exists($path)) {
+            unlink($path);
+        }
+
+        $path = "custom/Espo/Custom/Resources/layouts/Channel/detailSmall.json";
+        if (file_exists($path)) {
+            unlink($path);
+        }
+
+        $this->rebuild();
+        $this->updateComposer('atrocore/pim', '^1.10.7');
     }
 
     public function down(): void
     {
+        throw new \Error("Downgrade is prohibited.");
     }
 }
