6813445bbe582139e3be8905da259c0b1d04ff4d
Raoul Dzoukou <rd@atrocore.com>
2023-12-29 16:10:22 +0100

    .

--- app/Migrations/V1Dot11Dot2.php
+++ app/Migrations/V1Dot11Dot2.php
@@ -20,56 +20,7 @@ class V1Dot11Dot2 extends Base
 {
     public function up(): void
     {
-        // fetch currencies in the system
-        $units = $this->getConnection()->createQueryBuilder()
-            ->select(['name', 'id'])
-            ->from('unit')
-            ->where('measure_id=:id')
-            ->setParameter('id', 'currency')
-            ->fetchAllKeyValue();
-
-        // add unit column
-        $fromSchema = $this->getCurrentSchema();
-        $toSchema = clone $fromSchema;
-
-        $this->addColumn($toSchema, 'product', 'price_unit_id', ['type' => 'string', 'default' => null]);
-
-        foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
-            $this->getPDO()->exec($sql);
-        }
-
-        // migrate price data
-        $limit = 2000;
-        $offset = 0;
-
-        while (true) {
-
-            $products = $this->getConnection()->createQueryBuilder()
-                ->from('product')
-                ->select(['id', 'price', 'price_currency'])
-                ->where('price_currency is not null')
-                ->setMaxResults($limit)
-                ->setFirstResult($offset)
-                ->fetchAllAssociative();
-
-            if (empty($products)) {
-                break;
-            }
-
-            $offset = $offset + $limit;
-
-            foreach ($products as $product) {
-                $unitId = $units[$product['price_currency']] ?? $product['price_currency'];
-
-                $this->getConnection()->createQueryBuilder()
-                    ->update('product')
-                    ->set('price_unit_id', ':value')
-                    ->where('id = :id')
-                    ->setParameter('id', $product['id'])
-                    ->setParameter('value', $unitId)
-                    ->executeStatement();
-            }
-        }
+        \Atro\Migrations\V1Dot8Dot3::migrateCurrencyField($this, 'Product', 'price', 'currency');
 
         // change currency type to float with measure
         $this->getConnection()->createQueryBuilder()
@@ -83,6 +34,7 @@ class V1Dot11Dot2 extends Base
             ->executeStatement();
 
         // move varchar_value to reference_value, change names with unit ids , change type of pavs
+        $limit = 2000;
         $offset = 0;
 
         while (true) {
@@ -123,32 +75,6 @@ class V1Dot11Dot2 extends Base
                     ->executeStatement();
             }
         }
-
-        // remove currency column
-        $fromSchema = $this->getCurrentSchema();
-        $toSchema = clone $fromSchema;
-
-        $this->dropColumn($toSchema, 'product', 'price_currency');
-
-        foreach ($this->schemasDiffToSql($fromSchema, $toSchema) as $sql) {
-            $this->getPDO()->exec($sql);
-        }
-
-        // change price field in layouts
-        $dir = "custom/Espo/Custom/Resources/layouts/Product";
-        $files = scandir($dir);
-        foreach ($files as $file) {
-            if (in_array($file, array(".", ".."))) {
-                continue;
-            }
-            $path = "$dir/$file";
-            if (file_exists($path)) {
-                $contents = file_get_contents($path);
-                $contents = str_replace('"price"', '"unitPrice"', $contents);
-                file_put_contents($path, $contents);
-            }
-        }
-
     }
 
     public function down(): void
