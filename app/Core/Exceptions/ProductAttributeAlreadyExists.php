d88f4e332397479027760a212f4830a6a4da22e8
Roman Ratsun <rr@atrocore.com>
2024-02-29 14:50:26 +0000

    .

--- app/Core/Exceptions/ProductAttributeAlreadyExists.php
+++ app/Core/Exceptions/ProductAttributeAlreadyExists.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace Pim\Core\Exceptions;
 
-use Espo\Core\Exceptions\BadRequest;
+use Atro\Core\Exceptions\BadRequest;
 
 /**
  * Class ProductAttributeAlreadyExists
