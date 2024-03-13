6c2fd580c2dac3f0ab8dd27f1a32039b4409c616
Roman Ratsun <rr@atrocore.com>
2024-03-13 07:35:39 +0000

    .

--- client/modules/pim/src/views/settings/edit.js
+++ client/modules/pim/src/views/settings/edit.js
@@ -4,7 +4,7 @@
  * This source file is available under GNU General Public License version 3 (GPLv3).
  * Full copyright and license information is available in LICENSE.txt, located in the root directory.
  *
- * @copyright  Copyright (c) AtroCore UG (https://www.atrocore.com)
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
  * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
