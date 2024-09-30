264e06e193364dba4f0caccb6a629bb4d58eeab8
dzoukou <dzoukouraoul@gmail.com>
2024-09-30 10:35:47 +0100

    .

--- app/Migrations/V1Dot13Dot35.php
+++ app/Migrations/V1Dot13Dot35.php
@@ -34,12 +34,13 @@ class V1Dot13Dot35 extends Base
             $this->exec("CREATE INDEX IDX_CHANNEL_CLASSIFICATION_CREATED_AT ON channel_classification (created_at, deleted);");
             $this->exec("CREATE INDEX IDX_CHANNEL_CLASSIFICATION_MODIFIED_AT ON channel_classification (modified_at, deleted)");
         } else {
-
+            $this->exec("CREATE TABLE channel_classification (id VARCHAR(36) NOT NULL, deleted TINYINT(1) DEFAULT '0', created_at DATETIME DEFAULT NULL, modified_at DATETIME DEFAULT NULL, created_by_id VARCHAR(36) DEFAULT NULL, modified_by_id VARCHAR(36) DEFAULT NULL, channel_id VARCHAR(36) DEFAULT NULL, classification_id VARCHAR(36) DEFAULT NULL, UNIQUE INDEX IDX_CHANNEL_CLASSIFICATION_UNIQUE_RELATION (deleted, channel_id, classification_id), INDEX IDX_CHANNEL_CLASSIFICATION_CREATED_BY_ID (created_by_id, deleted), INDEX IDX_CHANNEL_CLASSIFICATION_MODIFIED_BY_ID (modified_by_id, deleted), INDEX IDX_CHANNEL_CLASSIFICATION_CHANNEL_ID (channel_id, deleted), INDEX IDX_CHANNEL_CLASSIFICATION_CLASSIFICATION_ID (classification_id, deleted), INDEX IDX_CHANNEL_CLASSIFICATION_CREATED_AT (created_at, deleted), INDEX IDX_CHANNEL_CLASSIFICATION_MODIFIED_AT (modified_at, deleted), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;");
         }
     }
 
     public function down(): void
     {
+        $this->exec("DROP TABLE channel_classification");
     }
 
     protected function exec(string $sql): void
