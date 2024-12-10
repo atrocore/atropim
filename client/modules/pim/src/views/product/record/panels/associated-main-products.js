4dc5f5cd394707c3cc4235d3f4c937618ba3333b
Kevin Teguia <kt@atrocore.com>
2024-12-10 13:24:13 +0100

    update associated product

--- client/modules/pim/src/views/product/record/panels/associated-main-products.js
+++ client/modules/pim/src/views/product/record/panels/associated-main-products.js
@@ -8,39 +8,164 @@
  * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
-Espo.define('pim:views/product/record/panels/associated-main-products', ['pim:views/record/panels/records-in-groups'],
-    (Dep) => Dep.extend({
+Espo.define('pim:views/product/record/panels/associated-main-products', ['views/record/panels/relationship', 'views/record/panels/bottom'],
+    (Dep, BottomPanel) => Dep.extend({
         groupScope: 'Association',
+
         disableSelect: true,
-        rowActionsView: 'views/record/row-actions/relationship-no-unlink',
+
+        template: 'pim:product/record/panels/associated-main-products',
+
+        rowActionsView: 'views/record/row-actions/relationship-no-remove',
+
+        groups: [],
+
+        setup() {
+            let bottomPanel = new BottomPanel();
+            bottomPanel.setup.call(this);
+
+            this.link = this.link || this.defs.link || this.panelName;
+
+            if (!this.scope && !(this.link in this.model.defs.links)) {
+                throw new Error('Link \'' + this.link + '\' is not defined in model \'' + this.model.name + '\'');
+            }
+            this.title = this.title || this.translate(this.link, 'links', this.model.name);
+            this.scope = this.scope || this.model.defs.links[this.link].entity;
+            this.layoutName = 'listSmall';
+            if (this.checkAclAction('create')) {
+                this.buttonList.push({
+                    title: 'Create',
+                    action: this.defs.createAction || 'createRelated',
+                    link: this.link,
+                    acl: 'create',
+                    aclScope: this.scope,
+                    html: '<span class="fas fa-plus"></span>',
+                    data: {
+                        link: this.link,
+                        tabId: this.defs.tabId
+                    }
+                });
+            }
+
+            if (!this.disableDeleteAll && this.checkAclAction('delete')) {
+                this.actionList.push({
+                    label: 'deleteAll',
+                    action: 'deleteAllRelationshipEntities',
+                    data: {
+                        "relationshipScope": this.scope
+                    },
+                    acl: 'delete',
+                    aclScope: this.scope
+                });
+            }
+
+            this.listenTo(this.model, 'after:unrelate', () => {
+                this.actionRefresh();
+            });
+
+            this.listenTo(this.model, 'after:relate', (link) => {
+                if(link === 'associatedMainProducts') {
+                    this.actionRefresh();
+                }
+            });
+
+            this.setupListLayout();
+            this.wait(true);
+
+            this.getCollectionFactory().create(this.scope, collection => {
+                this.collection = collection;
+                this.fetchCollectionGroups(() => this.wait(false));
+            });
+        },
+
+        data() {
+            return _.extend({
+                groups: this.groups,
+                groupScope: this.groupScope
+            }, Dep.prototype.data.call(this));
+        },
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+            this.buildGroups();
+        },
+
+        actionRefresh() {
+            this.fetchCollectionGroups(() => {
+                this.reRender();
+            })
+        },
+
+        buildGroups() {
+            if (!this.groups || this.groups.length < 1) {
+                return;
+            }
+
+            this.groups.forEach((group, key) => {
+                this.getHelper().layoutManager.get('Product', this.layoutName, layout => {
+                    let list = [];
+                    layout.forEach(item => {
+                        if (item.name) {
+                            let field = item.name;
+                            let fieldType = this.getMetadata().get(['entityDefs', 'Product', 'fields', field, 'type']);
+                            if (fieldType) {
+                                this.getFieldManager().getAttributeList(fieldType, field).forEach(attribute => {
+                                    list.push(attribute);
+                                });
+                            }
+                        }
+                    });
+
+                    this.getCollectionFactory().create('Product', groupCollection => {
+                        this.initGroupCollection(group, groupCollection, () => {
+                            groupCollection.data.select = list.join(',')
+                            groupCollection.fetch().then(() => {
+                                let viewName = this.defs.recordListView || this.getMetadata().get('clientDefs.' + this.scope + '.recordViews.list') || 'Record.List';
+                                let options = {
+                                    scope: 'Product',
+                                    collection: groupCollection,
+                                    layoutName: this.layoutName,
+                                    listLayout: this.prepareListLayout(layout),
+                                    rowActionsView: 'views/record/row-actions/relationship-no-unlink',
+                                    checkboxes: false,
+                                    buttonsDisabled: true,
+                                    showMore: false,
+                                    el: `${this.options.el} .group[data-name="${group.key}"] .list-container`,
+                                };
+
+                                this.createView('associatedProduct' + group.key, viewName, options, view => {
+                                    view.render();
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        },
 
         initGroupCollection(group, groupCollection, callback) {
-            groupCollection.url = 'AssociatedProduct';
-            groupCollection.data.select = 'associationId,sorting';
-            groupCollection.data.tabId = this.defs.tabId;
+            groupCollection.url = 'Product';
+            groupCollection.collectionOnly = true;
+            groupCollection.maxSize = 999
             groupCollection.where = [
                 {
-                    type: 'equals',
-                    attribute: 'mainProductId',
-                    value: this.model.id
-                },
-                {
-                    type: 'equals',
-                    attribute: 'associationId',
-                    value: group.key
+                    type: 'linkedWith',
+                    attribute: 'associatedRelatedProduct',
+                    subQuery: [
+                        {
+                            type: 'equals',
+                            attribute: 'associationId',
+                            value: group.id
+                        },
+                        {
+                            type: 'equals',
+                            attribute: 'mainProductId',
+                            value: this.model.id
+                        }
+                    ]
                 }
-            ];
-            groupCollection.maxSize = 9999;
-
-            groupCollection.fetch().success(() => {
-                groupCollection.forEach(item => {
-                    if (this.collection.get(item.get('id'))) {
-                        this.collection.remove(item.get('id'));
-                    }
-                    this.collection.add(item);
-                });
-                callback();
-            });
+            ]
+            callback();
         },
 
         fetchCollectionGroups(callback) {
@@ -61,6 +186,14 @@ Espo.define('pim:views/product/record/panels/associated-main-products', ['pim:vi
                 this.groups = data.list.map(row => ({id: row.id, key: row.id, label: row.name}));
                 callback();
             });
+        },
+
+        prepareListLayout(layout) {
+            layout.forEach((v, k) => {
+                layout[k]['notSortable'] = true;
+            });
+
+            return layout;
         },
 
         deleteEntities(groupId) {
@@ -99,7 +232,7 @@ Espo.define('pim:views/product/record/panels/associated-main-products', ['pim:vi
             });
         },
 
-        unlinkGroup(data) {
+        actionUnlinkGroup(data) {
             let id = data.id;
             if (!id) {
                 return;
@@ -118,6 +251,10 @@ Espo.define('pim:views/product/record/panels/associated-main-products', ['pim:vi
                 this.deleteEntities(group.id)
             }, this);
         },
+
+        checkAclAction(action) {
+            return this.getAcl().check(this.scope, action);
+        }
 
     })
 );
