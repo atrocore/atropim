454e97ba70ab9cce67f11c79aa6d9c641931f5ce
Roman Ratsun <roman.ratsun@atrocore.com>
2025-04-29 14:52:16 +0000

    .

---
+++ client/modules/pim/src/views/attribute/record/panels/nested-attributes.js
@@ -0,0 +1,24 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('pim:views/attribute/record/panels/nested-attributes', 'views/record/panels/relationship',
+    Dep => Dep.extend({
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+
+            this.$el.parent().hide();
+            if (this.model.get('type') === 'composite') {
+                this.$el.parent().show();
+            }
+        }
+
+    })
+);
