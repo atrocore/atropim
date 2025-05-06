0b8e58995e258466cf4fab95be0759e231fc84ce
Roman Ratsun <roman.ratsun@atrocore.com>
2025-05-06 08:01:24 +0000

    .

---
+++ client/modules/pim/src/views/entity/fields/attributes-disabled.js
@@ -0,0 +1,32 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('pim:views/entity/fields/attributes-disabled', 'views/fields/bool',
+    Dep => Dep.extend({
+
+        afterRender() {
+            Dep.prototype.afterRender.call(this);
+
+            const scope = this.model.id;
+
+            this.$el.parent().hide();
+            if (
+                !this.model.isNew()
+                && !this.getMetadata().get(`scopes.${scope}.hideAttributesDisabled`)
+                && !this.getMetadata().get(`scopes.${scope}.emHidden`)
+                && ['Base', 'Hierarchy'].includes(this.getMetadata().get(`scopes.${scope}.type`))
+            ) {
+                this.$el.parent().show();
+            }
+        },
+
+    })
+);
+
