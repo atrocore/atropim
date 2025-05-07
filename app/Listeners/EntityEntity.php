78d3c93b6c2e3e62ec6c8435bd649216005709bf
Roman Ratsun <roman.ratsun@atrocore.com>
2025-05-07 09:32:55 +0000

    .

---
+++ app/Listeners/EntityEntity.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Listeners;
+
+use Atro\Core\EventManager\Event;
+use Atro\Core\Exceptions\BadRequest;
+use Espo\ORM\Entity;
+
+class EntityEntity extends AbstractEntityListener
+{
+    public function beforeSave(Event $event): void
+    {
+        /** @var Entity $entity */
+        $entity = $event->getArgument('entity');
+
+        if ($entity->isAttributeChanged('hasAttribute') && empty($entity->get('hasAttribute'))) {
+            $attribute = $this->getEntityManager()->getRepository('Attribute')
+                ->where(['entityId' => $entity->id])
+                ->findOne();
+
+            if (!empty($attribute)) {
+                throw new BadRequest($this->getLanguage()->translate('entityAlreadyHasAttributesInUse', 'exceptions', 'Entity'));
+            }
+        }
+    }
+}
