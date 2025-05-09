2b2288067c53df56495125eba46872a37caaf123
Roman Ratsun <roman.ratsun@atrocore.com>
2025-05-09 08:56:46 +0000

    .

--- app/Migrations/V1Dot14Dot3.php
+++ app/Migrations/V1Dot14Dot3.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace Pim\Migrations;
 
 use Atro\Core\Migration\Base;
+use Atro\ORM\DB\RDB\Mapper;
+use Doctrine\DBAL\ParameterType;
 
 class V1Dot14Dot3 extends Base
 {
@@ -47,12 +49,192 @@ class V1Dot14Dot3 extends Base
 
         foreach ($this->getConfig()->get('inputLanguageList', []) as $language) {
             $this->exec("ALTER TABLE product_attribute_value ADD varchar_value_" . strtolower($language) . " VARCHAR(255) DEFAULT NULL");
-            $this->exec("ALTER TABLE product_attribute_value ADD text_value_" . strtolower($language) . " VARCHAR(255) DEFAULT NULL");
+            $this->exec("ALTER TABLE product_attribute_value ADD text_value_" . strtolower($language) . " TEXT DEFAULT NULL");
         }
 
+        $this->exec("ALTER TABLE attribute ADD channel_id VARCHAR(36) DEFAULT NULL");
+        $this->exec("CREATE INDEX IDX_ATTRIBUTE_CHANNEL_ID ON attribute (channel_id, deleted)");
+
+        $this->exec("ALTER TABLE product_attribute_value DROP attribute_type");
+        $this->exec("ALTER TABLE product_attribute_value DROP created_at");
+        $this->exec("ALTER TABLE product_attribute_value DROP modified_at");
+        $this->exec("ALTER TABLE product_attribute_value DROP created_by_id");
+        $this->exec("ALTER TABLE product_attribute_value DROP modified_by_id");
+        $this->exec("ALTER TABLE product_attribute_value DROP count_bytes_instead_of_characters");
+
+        $this->migrateArrayType();
+        $this->migrateMultilang();
+        $this->migrateChannelSpecific();
+
+        echo '<pre>';
+        print_r('22222');
+        die();
+
         // migrate data
+        // migrate variant_specific_attribute
 
         $this->exec("CREATE UNIQUE INDEX IDX_PRODUCT_ATTRIBUTE_VALUE_UNIQUE_RELATIONSHIP ON product_attribute_value (deleted, product_id, attribute_id)");
+    }
+
+    protected function migrateChannelSpecific(): void
+    {
+        while (true) {
+            try {
+                $res = $this->getConnection()->createQueryBuilder()
+                    ->select('*')
+                    ->from('product_attribute_value')
+                    ->where('deleted=:false')
+                    ->andWhere('channel_id IS NOT NULL AND channel_id !=:empty')
+                    ->setFirstResult(0)
+                    ->setMaxResults(5000)
+                    ->setParameter('false', false, ParameterType::BOOLEAN)
+                    ->setParameter('empty', '')
+                    ->fetchAllAssociative();
+            } catch (\Throwable $e) {
+                return;
+            }
+
+            if (empty($res)) {
+                break;
+            }
+
+            foreach ($res as $item) {
+                $attribute = $this->getConnection()->createQueryBuilder()
+                    ->select('*')
+                    ->from($this->getConnection()->quoteIdentifier('attribute'))
+                    ->where('id=:id')
+                    ->setParameter('id', $item['attribute_id'])
+                    ->fetchAssociative();
+
+                if (empty($attribute)) {
+                    continue;
+                }
+
+                $attributeId = md5($attribute['id'] . '_' . $item['channel_id']);
+
+                $qb = $this->getConnection()->createQueryBuilder()
+                    ->insert($this->getConnection()->quoteIdentifier('attribute'));
+
+                foreach ($attribute as $column => $val) {
+                    if ($column === 'id') {
+                        $qb->setValue('id', ':id')
+                            ->setParameter('id', $attributeId);
+                    } elseif ($column === 'channel_id') {
+                        $qb->setValue('channel_id', ':channelId')
+                            ->setParameter('channelId', $item['channel_id']);
+                    } else {
+                        $qb->setValue($this->getConnection()->quoteIdentifier($column), ":$column")
+                            ->setParameter($column, $val, Mapper::getParameterType($val));
+                    }
+                }
+
+                try {
+                    $qb->executeQuery();
+                } catch (\Throwable $e) {
+                }
+
+                $this->getConnection()->createQueryBuilder()
+                    ->update('product_attribute_value')
+                    ->set('channel_id', ':empty')
+                    ->set('attribute_id', ':attributeId')
+                    ->where('id=:id')
+                    ->setParameter('empty', '')
+                    ->setParameter('attributeId', $attributeId)
+                    ->setParameter('id', $item['id'])
+                    ->executeQuery();
+            }
+        }
+
+        $this->exec("ALTER TABLE product_attribute_value DROP channel_id");
+    }
+
+    protected function migrateArrayType(): void
+    {
+        while (true) {
+            $res = $this->getConnection()->createQueryBuilder()
+                ->select('av.*')
+                ->from('product_attribute_value', 'av')
+                ->leftJoin('av', $this->getConnection()->quoteIdentifier('attribute'), 'a',
+                    'a.id=av.attribute_id AND a.deleted=:false')
+                ->where('a.type IN (:types)')
+                ->andWhere('av.deleted=:false')
+                ->andWhere('av.text_value IS NOT NULL')
+                ->setParameter('false', false, ParameterType::BOOLEAN)
+                ->setParameter('types', ['extensibleMultiEnum', 'array'], $this->getConnection()::PARAM_STR_ARRAY)
+                ->setFirstResult(0)
+                ->setMaxResults(5000)
+                ->fetchAllAssociative();
+
+            if (empty($res)) {
+                break;
+            }
+
+            foreach ($res as $item) {
+                $this->getConnection()->createQueryBuilder()
+                    ->update('product_attribute_value')
+                    ->set('json_value', ':jsonValue')
+                    ->set('text_value', ':null')
+                    ->where('id=:id')
+                    ->setParameter('jsonValue', $item['text_value'])
+                    ->setParameter('null', null, ParameterType::NULL)
+                    ->setParameter('id', $item['id'])
+                    ->executeQuery();
+            }
+        }
+    }
+
+    protected function migrateMultilang(): void
+    {
+        while (true) {
+            try {
+                $res = $this->getConnection()->createQueryBuilder()
+                    ->select('*')
+                    ->from('product_attribute_value')
+                    ->where('language!=:main')
+                    ->andWhere('deleted=:false')
+                    ->setFirstResult(0)
+                    ->setMaxResults(5000)
+                    ->setParameter('main', 'main')
+                    ->setParameter('false', false, ParameterType::BOOLEAN)
+                    ->fetchAllAssociative();
+            } catch (\Throwable $e) {
+                return;
+            }
+
+            if (empty($res)) {
+                break;
+            }
+
+            foreach ($res as $record) {
+                // move value to lingual column
+                $this->getConnection()->createQueryBuilder()
+                    ->update('product_attribute_value')
+                    ->set('varchar_value_' . strtolower($record['language']), ':varcharValue')
+                    ->set('text_value_' . strtolower($record['language']), ':textValue')
+                    ->where('language=:main')
+                    ->andWhere('product_id=:productId')
+                    ->andWhere('attribute_id=:attributeId')
+                    ->andWhere('channel_id=:channelId')
+                    ->andWhere('deleted=:false')
+                    ->setParameter('varcharValue', $record['varchar_value'])
+                    ->setParameter('textValue', $record['text_value'])
+                    ->setParameter('main', 'main')
+                    ->setParameter('productId', $record['product_id'])
+                    ->setParameter('channelId', $record['channel_id'])
+                    ->setParameter('attributeId', $record['attribute_id'])
+                    ->setParameter('false', false, ParameterType::BOOLEAN)
+                    ->executeQuery();
+
+                // delete lingual record
+                $this->getConnection()->createQueryBuilder()
+                    ->delete('product_attribute_value')
+                    ->where('id=:id')
+                    ->setParameter('id', $record['id'])
+                    ->executeQuery();
+            }
+        }
+
+        $this->exec("ALTER TABLE product_attribute_value DROP " . $this->getConnection()->quoteIdentifier('language'));
     }
 
     protected function exec(string $query): void
