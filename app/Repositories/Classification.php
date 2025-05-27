b34bf3a5ed355b892548a1f1ef18a1f735832f1b
yurdm <yd@atrocore.com>
2025-05-27 17:11:31 +0300

    .

--- app/Repositories/Classification.php
+++ app/Repositories/Classification.php
@@ -90,29 +90,36 @@ class Classification extends Hierarchy
 
     protected function beforeRemove(Entity $entity, array $options = [])
     {
-        if (!empty($entity->get('entityId'))) {
-            $relTable = $this->getMapper()->toDb($entity->get('entityId') . 'Classification');
-            $entityColumn = $this->getMapper()->toDb(lcfirst("{$entity->get('entityId')}Id"));
-            $entityTable = $this->getMapper()->toDb($entity->get('entityId'));
-
-            $record = $this->getConnection()->createQueryBuilder()
-                ->select('ec.id')
-                ->from($relTable, 'ec')
-                ->innerJoin('ec', $entityTable, 'e', "e.id = ec.$entityColumn AND e.deleted = :false")
-                ->where('ec.classification_id = :classification_id')
-                ->andWhere('ec.deleted = :false')
-                ->setParameter('classification_id', $entity->get('id'))
-                ->setParameter('false', false, \Doctrine\DBAL\ParameterType::BOOLEAN)
-                ->setMaxResults(1)
-                ->executeQuery()
-                ->fetchAssociative();
-
-            if (!empty($record)) {
-                throw new BadRequest($this->getLanguage()->translate('classificationHasRecords', 'exceptions', 'Classification'));
-            }
+        if ($this->hasRelatedRecords($entity)) {
+            throw new BadRequest($this->getLanguage()->translate('classificationHasRecords', 'exceptions', 'Classification'));
         }
 
         parent::beforeRemove($entity, $options);
+    }
+
+    protected function hasRelatedRecords(Entity $entity): bool
+    {
+        if (empty($entity->get('entityId'))) {
+            return false;
+        }
+
+        $relTable = $this->getMapper()->toDb(lcfirst($entity->get('entityId') . 'Classification'));
+        $entityColumn = $this->getMapper()->toDb(lcfirst($entity->get('entityId') . 'Id'));
+        $entityTable = $this->getMapper()->toDb(lcfirst($entity->get('entityId')));
+
+        $record = $this->getConnection()->createQueryBuilder()
+            ->select('ec.id')
+            ->from($relTable, 'ec')
+            ->innerJoin('ec', $entityTable, 'e', "e.id = ec.$entityColumn AND e.deleted = :false")
+            ->where('ec.classification_id = :classification_id')
+            ->andWhere('ec.deleted = :false')
+            ->setParameter('classification_id', $entity->get('id'))
+            ->setParameter('false', false, \Doctrine\DBAL\ParameterType::BOOLEAN)
+            ->setMaxResults(1)
+            ->executeQuery()
+            ->fetchAssociative();
+
+        return !empty($record);
     }
 
     public function remove(Entity $entity, array $options = [])
