f6c62acffb1807eeb5dcce1016ae548ab8d0e753
Roman Ratsun <roman.ratsun@atrocore.com>
2025-06-16 12:29:42 +0000

    .

---
+++ client/modules/pim/src/views/role-scope-attribute-panel/fields/attribute-panel.js
@@ -0,0 +1,25 @@
+/**
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+Espo.define('pim:views/role-scope-attribute-panel/fields/attribute-panel', 'views/fields/link', Dep => {
+
+    return Dep.extend({
+
+        selectBoolFilterList: ['onlyForEntity'],
+
+        boolFilterData: {
+            onlyForEntity() {
+                return this.model.get('roleScopeName') || null;
+            }
+        },
+
+    });
+});
+
