7b4f580f40dd481ccdf5ce599de471dfed90f1c4
Kevin Teguia <kevin.teguia@atrocore.com>
2025-06-27 11:36:04 +0100

    add entityId as mandatory

--- app/Services/Attribute.php
+++ app/Services/Attribute.php
@@ -27,7 +27,7 @@ use Espo\ORM\Entity;
 
 class Attribute extends Base
 {
-    protected $mandatorySelectAttributeList = ['sortOrder', 'attributeGroupSortOrder', 'extensibleEnumId', 'data', 'measureId', 'defaultUnit'];
+    protected $mandatorySelectAttributeList = ['sortOrder', 'attributeGroupSortOrder', 'extensibleEnumId', 'data', 'measureId', 'defaultUnit', 'entityId'];
 
     public function getAttributesDefs(string $entityName, array $attributesIds): array
     {
