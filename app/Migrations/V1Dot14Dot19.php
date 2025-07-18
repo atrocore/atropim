8de58ac74ff20183a7bcae7ea63571833f59bf54
yurdm <yd@atrocore.com>
2025-07-18 07:22:24 +0000

    Update product preview template

---
+++ app/Migrations/V1Dot14Dot19.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+declare(strict_types=1);
+
+namespace Pim\Migrations;
+
+use Atro\Core\Migration\Base;
+
+class V1Dot14Dot19 extends Base
+{
+    public function getMigrationDateTime(): ?\DateTime
+    {
+        return new \DateTime('2025-07-18 09:00:00');
+    }
+
+    public function up(): void
+    {
+        $seeder = new \Pim\Seeders\PreviewSeeder($this->getConfig(), $this->getConnection());
+
+        foreach ($seeder->getPreviewTemplates() as $previews) {
+            if (empty($previews['template']) || empty($previews['id'])) {
+                continue;
+            }
+
+            try {
+                $this->getConnection()->createQueryBuilder()
+                    ->update('preview_template')
+                    ->set('template', ':template')
+                    ->where('id = :id')
+                    ->setParameter('template', $previews['template'])
+                    ->setParameter('id', $previews['id'])
+                    ->executeStatement();
+            } catch (\Throwable $e) {}
+        }
+    }
+}
---
+++ app/Seeders/PreviewSeeder.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * AtroCore Software
+ *
+ * This source file is available under GNU General Public License version 3 (GPLv3).
+ * Full copyright and license information is available in LICENSE.txt, located in the root directory.
+ *
+ * @copyright  Copyright (c) AtroCore GmbH (https://www.atrocore.com)
+ * @license    GPLv3 (https://www.gnu.org/licenses/)
+ */
+
+namespace Pim\Seeders;
+
+use Atro\Core\Utils\Util;
+use Atro\Seeders\AbstractSeeder;
+
+class PreviewSeeder extends AbstractSeeder
+{
+    public function run(): void
+    {
+        foreach ($this->getPreviewTemplates() as $preview) {
+            try {
+                $this->getConnection()->createQueryBuilder()
+                    ->insert('preview_template')
+                    ->setValue('id', ':id')
+                    ->setValue('deleted', ':false')
+                    ->setValue('name', ':name')
+                    ->setValue('entity_type', ':entityType')
+                    ->setValue('is_active', ':active')
+                    ->setValue('template', ':template')
+                    ->setParameter('id', $preview['id'] ?? Util::generateId())
+                    ->setParameter('false', false, \Doctrine\DBAL\ParameterType::BOOLEAN)
+                    ->setParameter('entityType', $preview['entityType'])
+                    ->setParameter('name', $preview['name'])
+                    ->setParameter('active', $preview['active'], \Doctrine\DBAL\ParameterType::BOOLEAN)
+                    ->setParameter('template', $preview['template'])
+                    ->executeStatement();
+            } catch (\Throwable $e) {
+            }
+        }
+    }
+
+    public function getPreviewTemplates(): array
+    {
+        $module = new \ReflectionClass(\Pim\Module::class);
+        $templateDir = dirname($module->getFileName()) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
+
+        return [
+            [
+                'id'         => 'product_preview',
+                'name'       => 'Product preview',
+                'template'   => file_get_contents($templateDir . 'Product' . DIRECTORY_SEPARATOR . 'template.twig'),
+                'active'     => true,
+                'entityType' => 'Product',
+            ],
+        ];
+    }
+}
